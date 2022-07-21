bfb1d342585e5a53d9ae15c4d10cea072d97bb9d
m3tra <fporto@student.42lisboa.com>
2022-07-22 00:21:58 +0100

    Update delivery.js

--- delivery.js
+++ delivery.js
@@ -1,1 +1,13 @@
-
+/** @param {NS} ns */
+export async function main(ns) {
+	var target;
+	var deliveryFiles;
+	
+	if (!ns.args[0] || ns.args.length != 1) {
+		ns.tprint("Required target hostname as only argument");
+		return;
+	}
+	target = ns.args[0];
+	deliveryFiles = ns.ls("home", "Delivery");
+	await ns.scp(deliveryFiles, target);
+}

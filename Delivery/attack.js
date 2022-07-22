7eaba8c6364600a8d6cf7f84176376509ee96c5d
m3tra <fporto@student.42lisboa.com>
2022-07-22 16:52:10 +0100

    Update attack.js

--- Delivery/attack.js
+++ Delivery/attack.js
@@ -1,26 +1,32 @@
 /** @param {NS} ns */
 export async function weaken(ns, hostname) {
-	var minSec = ns.getServerMinSecurityLevel(hostname);
-	var currSec = ns.getServerSecurityLevel(hostname);
+	let minSec = ns.getServerMinSecurityLevel(hostname);
+	let currSec = ns.getServerSecurityLevel(hostname);
+	let prediction = ns.weakenAnalyze(1);
 
-	if (currSec != minSec) {
-		ns.print("Weakening...");
-	}
+	ns.print("prediction: " + prediction);
+	if (currSec - prediction < minSec)
+		return;
 	while (currSec > minSec) {
+		ns.print("Weakening...");
 		currSec = ns.getServerSecurityLevel(hostname);
 		await ns.weaken(hostname);
 	}
 }
 
 export async function grow(ns, hostname) {
-	var maxMoney = ns.getServerMaxMoney(hostname);
-	var currMoney = ns.getServerMoneyAvailable(hostname);
+	let maxMoney = ns.getServerMaxMoney(hostname);
+	let currMoney = ns.getServerMoneyAvailable(hostname);
+	let growthParam = ns.getServerGrowth(hostname);
 
-	if (currMoney != maxMoney)
-		ns.print("Growing...");
+	ns.print("growthParam: " + growthParam);
+	if (currMoney * (growthParam / 100) > maxMoney)
+		return;
 	while (currMoney < maxMoney) {
+		ns.print("Growing...");
 		currMoney = ns.getServerMoneyAvailable(hostname);
 		await ns.grow(hostname);
+		await weaken(ns, hostname);
 	}
 }
 
